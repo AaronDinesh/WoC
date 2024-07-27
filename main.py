41d7fbdacaf8f0ad367e1d399d55b541ab92fd74
Aaron Dinesh <aarondinesh2002@gmail.com>
2024-07-27 16:47:16 +0100

    Fixed bugs with randomisation and langauge localization in downloadnig transcript

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import networkx as nx
 from youtubeAPI import youtubeAPI
 import woc_utils as wu
 import youtubeData as ytd
+import fnmatch
+import os
+import numpy as np
 
 def main():
     # wu.chunker("test.txt")
@@ -11,10 +14,20 @@ def main():
     # ret = client.authenticate()
     # client.downloadCaptions("eVli-tstM5E")
     # client.apiShutdown() 
+    
+    next_id = "A5w-dEgIU1M"
+    
+    while len(fnmatch.filter(os.listdir('captions/'), '*.txt')) < 100:
+        if(not os.path.exists(f"captions/{next_id}.txt")):
+            ytd.downloadCaptions(next_id)
+        else:
+            print(f"ID: {next_id} exists. Skipping...")
 
-    relatedIDs = ytd.randomYoutubeID("fcfQkxwz4Oo")
-    for id in relatedIDs: 
-        ytd.downloadCaptions(id)
+        relatedIDs = ytd.randomYoutubeID(next_id)
+        relatedIDs += ytd.randomYoutubeID()
+        
+        next_id = str(np.random.choice(relatedIDs, 1 ,replace=False)[0])
+
 
 if __name__ == "__main__":
     main()
--- youtubeData.py
+++ youtubeData.py
@@ -5,19 +5,20 @@ from bs4 import BeautifulSoup
 import re
 import json
 from tqdm import tqdm
+import numpy as np
 
 def randomYoutubeID(video_id: str = None):
-    
+
     if video_id == None:
-        url = f"https://www.youtube.com/"
+        url = "https://www.youtube.com/feed/trending"
     else:
         url = f"https://www.youtube.com/watch?v={video_id}"
-    
-    
+
+
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     related_ids = []
-    
+
     # Find the script tag containing the related videos data
     scripts = soup.find_all('script')
     for script in scripts:
@@ -28,39 +29,62 @@ def randomYoutubeID(video_id: str = None):
             data = data.strip()
             if data.endswith(';'):
                 data = data[:-1]
-        
-            
+
             try:
                 json_data = json.loads(data)
             except json.JSONDecodeError as e:
                 print(f"Error decoding JSON: {e}")
                 continue
-            
+
+
             # Extract related video IDs
             try:
-                related_videos = json_data['contents']['twoColumnWatchNextResults']['secondaryResults']['secondaryResults']['results']
-                for video in related_videos:
-                    if 'compactVideoRenderer' in video:
-                        video_id = video['compactVideoRenderer']['videoId']
-                        related_ids.append(video_id)
-                
+                if video_id == None:
+                    tabs = json_data['contents']['twoColumnBrowseResultsRenderer']['tabs']
+
+                    for tab in tabs:
+                        if len(related_ids) >= 10:
+                            break
+
+
+                        tab_contents = tab['tabRenderer']['content']['sectionListRenderer']['contents']
+                        print("not tab_contents")
+                        for tab_content in tab_contents:
+                            items = tab_content['itemSectionRenderer']['contents']
+
+                            for item in items:
+                                videos = item['shelfRenderer']['content']['expandedShelfContentsRenderer']['items']
+
+                                for video in videos:
+                                    videoID = video['videoRenderer']['videoId']
+                                    related_ids.append(videoID)
+                else:
+                    related_videos = json_data['contents']['twoColumnWatchNextResults']['secondaryResults']['secondaryResults']['results']
+                    for i in range(min(10, len(related_videos))):
+                        if 'compactVideoRenderer' in related_videos[i]:
+                            videoID = related_videos[i]['compactVideoRenderer']['videoId']
+                            related_ids.append(videoID)
+
             except KeyError as e:
                 print(f"Error accessing JSON structure: {e}")
                 continue
 
-    return related_ids
+    return np.random.choice(related_ids, 5, replace=False)
+
 
 def downloadCaptions(videoID: str):
     try:
         # Retrieving a list of dictionaries of the transcript
-        srt = YouTubeTranscriptApi.get_transcript(videoID)
+        srt = YouTubeTranscriptApi.get_transcript(videoID, languages=['en', 'en-US', 'en-AU', 'en-BZ', 'en-CA', 
+                                                                      'en-IE', 'en-JM', 'en-NZ', 'en-ZA', 'en-tt', 
+                                                                      'en-GB'])
 
         # Writing the transcripts to a .txt file
         with open(f"captions/{videoID}.txt", "w", encoding="utf-8") as f:
-        
+
             # iterating through each element of list srt
             for i in tqdm(srt, desc=f"Downloading ID: {videoID}"):
                 # writing each element of srt on a new line
                 f.write("{}\n".format(i["text"]))
-    except:
-        print("Error! Skipping videoID " + videoID)
+    except Exception as e:
+        print(f"Error: {e}. Skipping videoID " + videoID)
