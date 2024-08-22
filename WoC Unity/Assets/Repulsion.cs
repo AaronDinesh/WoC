525389aa48e4c6bfaad68fe21f7a7e01c56f9430
Aaron Dinesh <aarondinesh2002@gmail.com>
2024-08-22 21:26:39 +0100

    Tweaked repulsion script to contain an array of targets

--- WoC Unity/Assets/Repulsion.cs
+++ WoC Unity/Assets/Repulsion.cs
@@ -6,24 +6,46 @@ public class Repulsion : MonoBehaviour
 {
     public float G = 10.0f;
     public GameObject repeller;
-    public GameObject target;
+    public List<GameObject> targets = new List<GameObject>();
+    public List<float> dotProducts = new List<float>();
 
-    List<List<float>> embeddings = new List<List<float>>();
+    List<float> repellerEmbedding;
 
-    public float dotProduct;
-
-     // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        embeddings.Add(repeller.GetComponent<VideoProperties>().embedding);
-        embeddings.Add(target.GetComponent<VideoProperties>().embedding);
-
-        // float dotProduct = repeller.mass*target.mass;
-        for (int i=0; i < embeddings[0].Count; i++)
-        {
-            dotProduct = dotProduct + 100*embeddings[0][i]*embeddings[1][0];
-        }         
+        repellerEmbedding = repeller.GetComponent<VideoProperties>().embedding;
+        
+        foreach(GameObject target in targets){
+            float dotProduct = 0.0f;
+            List<float> targetEmbedding = target.GetComponent<VideoProperties>().embedding;
+             // float dotProduct = repeller.mass*target.mass;
+            for(int i = 0; i < repellerEmbedding.Count; i++){
+                dotProduct += repellerEmbedding[i] * targetEmbedding[i];
+            }
+            dotProducts.Add(dotProduct);
+        }
     }
+
+    void ColorTargets(){
+        
+        
+        for(int i = 0; i < targets.Count; i++){
+            MeshRenderer meshRendererTarget = targets[i].GetComponent<MeshRenderer>();
+            Material newMaterialTarget = new Material(Shader.Find("Standard"));
+        
+
+
+            if(dotProducts[i] > 0){
+                newMaterialTarget.color = new Color(0, 255, 0, 1);
+                meshRendererTarget.material = newMaterialTarget;
+            }else if(dotProducts[i] < 0){
+                newMaterialTarget.color = new Color(255, 0, 0, 1);
+                meshRendererTarget.material = newMaterialTarget;
+            }
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -38,45 +60,87 @@ public class Repulsion : MonoBehaviour
 
         // float dotProduct = float.Dot(embeddings[0], embeddings[1]);
 
-
-
         //float distance = Vector3.Distance(repeller.position,target.position.
-        Vector3 difference = repeller.GetComponent<Rigidbody>().position - target.GetComponent<Rigidbody>().position;
-        float distance = difference.magnitude; // r = Mathf.Sqrt((x*x)+(y*y))
 
-        // If the spheres are close together and attracted to each other, add the force
-        if (distance <= 200.0f && dotProduct > 0)
-        {
-            //F = G * ((m1*m2)/r^2)
-            // float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,2);
-            float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,2);
-            // float unScaledforceMagnitude = dotProduct/Mathf.Abs(Mathf.Log(distance));
-            float forceMagnitude = G*unScaledforceMagnitude;
+        float forceMagnitude = 0.0f;
+        Vector3 forceDirection = new Vector3(0.0f, 0.0f, 0.0f);
+        for(int i = 0; i < targets.Count; i++){
+            Vector3 difference = repeller.GetComponent<Rigidbody>().position - targets[i].GetComponent<Rigidbody>().position;
+            forceDirection += difference.normalized;
+            float distance = difference.magnitude;
 
-            Vector3 forceDirection = difference.normalized;
-
-            Vector3 forceVector = forceDirection*forceMagnitude;
-
-            target.GetComponent<Rigidbody>().AddForce(forceVector);
+            if(distance <= 200.0f && dotProducts[i] > 0){
+                forceMagnitude += (G*dotProducts[i])/Mathf.Pow(distance, 2);
+            }else if(dotProducts[i] < 0){
+                forceMagnitude += (G*dotProducts[i])/Mathf.Pow(distance, 1);
+            }
         }
-        // Else if the spheres that are repelled (close and far away), add the force
-        else if (dotProduct < 0)
-        {
-            //F = G * ((m1*m2)/r^2)
-            float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,1);
-            // float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,7);
-            // float unScaledforceMagnitude = dotProduct/Mathf.Abs(Mathf.Log(distance));
-            float forceMagnitude = G*unScaledforceMagnitude;
 
-            Vector3 forceDirection = difference.normalized;
+        Vector3 forceVector = forceMagnitude * forceDirection;
+        repeller.GetComponent<Rigidbody>().AddForce(forceVector);
 
-            Vector3 forceVector = forceDirection*forceMagnitude;
+        
+        // Vector3 difference = repeller.GetComponent<Rigidbody>().position - target.GetComponent<Rigidbody>().position;
+        // float distance = difference.magnitude; // r = Mathf.Sqrt((x*x)+(y*y))
 
-            target.GetComponent<Rigidbody>().AddForce(forceVector);
-        }
-        // Else the spheres are far away and attracted, do nothing
+        // // If the spheres are close together and attracted to each other, add the force
+        // if (distance <= 200.0f && dotProduct > 0)
+        // {
+        //     //F = G * ((m1*m2)/r^2)
+        //     // float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,2);
+        //     float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,2);
+        //     // float unScaledforceMagnitude = dotProduct/Mathf.Abs(Mathf.Log(distance));
+        //     float forceMagnitude = G*unScaledforceMagnitude;
+
+        //     Vector3 forceDirection = difference.normalized;
+
+        //     Vector3 forceVector = forceDirection*forceMagnitude;
+
+        //     target.GetComponent<Rigidbody>().AddForce(forceVector);
+        // }
+        // // Else if the spheres that are repelled (close and far away), add the force
+        // else if (dotProduct < 0)
+        // {
+        //     //F = G * ((m1*m2)/r^2)
+        //     float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,2);
+        //     // float unScaledforceMagnitude = dotProduct/Mathf.Pow(distance,7);
+        //     // float unScaledforceMagnitude = dotProduct/Mathf.Abs(Mathf.Log(distance));
+        //     float forceMagnitude = G*unScaledforceMagnitude;
+
+        //     Vector3 forceDirection = difference.normalized;
+
+        //     Vector3 forceVector = forceDirection*forceMagnitude;
+
+        //     target.GetComponent<Rigidbody>().AddForce(forceVector);
+        // }
+        // // Else the spheres are far away and attracted, do nothing
 
         // dotProduct = 0;
+        
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Your code here
+                Debug.Log("Clicked on: " + hit.collider.gameObject.name);
+                
+                MeshRenderer meshRendererRepeller = hit.collider.gameObject.GetComponent<MeshRenderer>();
+                Material newMaterialRepeller = new Material(Shader.Find("Standard"));
+                newMaterialRepeller.color = new Color(255, 255, 0, 1);
+                meshRendererRepeller.material = newMaterialRepeller;
+
+                
+                
+                
+                ColorTargets();
+            }
+        }
+
+
+
     }
 
 
--- WoC Unity/Assets/Spawner.cs
+++ WoC Unity/Assets/Spawner.cs
@@ -23,15 +23,21 @@ public class Spawner : MonoBehaviour
         var idEnumerator = embeddings.Keys.GetEnumerator();
         var embeddingEnumerator = embeddings.Values.GetEnumerator();
         
-        for (int i=0; i < embeddings.Count; i++)
-        {
-            float x = Random.Range(-10.0f, 10.0f);
-            float y = Random.Range(-10.0f, 10.0f);
-            float z = Random.Range(-10.0f, 10.0f);
-            Vector3 spawnPos = new Vector3(x, y, z);
-
+        
+        foreach(KeyValuePair<string, List<float>> entry in embeddings){
+            Vector3 spawnPos = new Vector3(entry.Value[0], entry.Value[1], entry.Value[2]);
             Instantiate(spherePrefab, spawnPos, Quaternion.identity);
         }
+        
+        // for (int i=0; i < embeddings.Count; i++)
+        // {
+        //     float x = Random.Range(-10.0f, 10.0f);
+        //     float y = Random.Range(-10.0f, 10.0f);
+        //     float z = Random.Range(-10.0f, 10.0f);
+        //     Vector3 spawnPos = new Vector3(x, y, z);
+
+        //     Instantiate(spherePrefab, spawnPos, Quaternion.identity);
+        // }
 
         gameObjects=GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
         foreach(GameObject go in gameObjects)
@@ -40,9 +46,9 @@ public class Spawner : MonoBehaviour
             if (go.name == "Sphere(Clone)")
             {
                 
-                Gravity scriptGravity = go.AddComponent<Gravity>(); // Add gravity to center
-                scriptGravity.attractor = GameObject.Find("Center").GetComponent<Rigidbody>();
-                scriptGravity.target = go.GetComponent<Rigidbody>();
+                //Gravity scriptGravity = go.AddComponent<Gravity>(); // Add gravity to center
+                //scriptGravity.attractor = GameObject.Find("Center").GetComponent<Rigidbody>();
+                //scriptGravity.target = go.GetComponent<Rigidbody>();
                 VideoProperties scriptVideoProperties = go.AddComponent<VideoProperties>();
                 
                 idEnumerator.MoveNext();
@@ -50,17 +56,15 @@ public class Spawner : MonoBehaviour
                 
                 scriptVideoProperties.id = idEnumerator.Current;
                 scriptVideoProperties.embedding = embeddingEnumerator.Current;
-                
+                Repulsion scriptRepulsion = go.AddComponent<Repulsion>();
+                scriptRepulsion.repeller = go;
+
                 // Loop through each other Sphere that isn't the current sphere
                 foreach(GameObject gameObj in gameObjects)
                 {
                     if (gameObj.name == "Sphere(Clone)" && gameObj != go)
                     {
-                        Repulsion scriptRepulsion = go.AddComponent<Repulsion>();
-                        scriptRepulsion.repeller = gameObj;
-                        scriptRepulsion.target = go;
-                        // scriptRepulsion.repeller = gameObj.GetComponent<Rigidbody>();
-                        // scriptRepulsion.target = go.GetComponent<Rigidbody>();
+                        scriptRepulsion.targets.Add(gameObj);
                     }
                 }
             }
