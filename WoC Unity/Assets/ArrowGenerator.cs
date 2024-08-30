a8be18887755cbd0d2c163a363cb0266a02bcf78
Aaron Dinesh <aarondinesh2002@gmail.com>
2024-08-30 13:43:28 +0200

    Worked on Arrow Mesh. Still need to figure out the rotation offset.

--- WoC Unity/Assets/ArrowGenerator.cs
+++ WoC Unity/Assets/ArrowGenerator.cs
@@ -21,7 +21,7 @@ public class ArrowGenerator : MonoBehaviour
         // Create a new mesh and assign it to the MeshFilter Component
         mesh = new Mesh();
         this.GetComponent<MeshFilter>().mesh = mesh;        
-        GenArrowBody(3.0f,4.0f);
+        GenArrow(0.25f, 1.0f, 0.5f, 0.33f, new Vector3(0,0,0), new Vector3(1, 0, 0));
     }
 
     // Update is called once per frame
@@ -36,25 +36,98 @@ public class ArrowGenerator : MonoBehaviour
         mesh.triangles = triangles;
     }
 
-    void GenArrowBody(float width, float height){
-        //Bottom center of the arrow body will be at (0, 0, 0)
+    void GenArrow(float BodyWidth, float BodyHeight, float TipWidth, float TipHeight, Vector3 start, Vector3 end){
         //Left-Right is x (Right +ve). Up-Down is y (Up +ve). Forward-Backward is z (Forward +ve).
         //Starting arrow at the bottom left and going clockwise
+        float theta = 0.0f;
+        float rho = 0.0f;
+        float gamma = 0.0f;
+
+        //arrow is made in the xy plane. There needs to be some constant offset
+        //to the angles below
+
+
+        if(end.x - start.x != 0){ 
+            //Angle between x and y axis. I.e. Rotation about Z axis
+            theta = Mathf.Atan((end.y - start.y)/(end.x - start.x));
+            //Angle between z and x. I.e. Rotation about the Y axis
+            rho = Mathf.Atan((end.z - start.z)/(end.x - start.x));
+        }
         
+        if(end.y - start.y != 0){
+            //Angle between z and y. I.e. Rotation about the X axis
+            gamma = Mathf.Atan((end.z - start.z)/(end.y - start.y));
+        }
+        Vector3 eulerAngleRotation = new Vector3(gamma, rho, theta) * Mathf.Rad2Deg;
+        Debug.Log(eulerAngleRotation);
+        
+        
+        float zOffset = 0.1f;
         //Bottom left
-        points.Add(new Vector3(-width/2, 0, 0));
+        points.Add(new Vector3(start.x - BodyWidth/2, start.y, start.z));
         
         //Top left
-        points.Add(new Vector3(-width/2, height, 0));
+        points.Add(new Vector3(start.x - BodyWidth/2, start.y + BodyHeight, start.z));
         
         //Top right
-        points.Add(new Vector3(width/2, height, 0));
+        points.Add(new Vector3(start.x + BodyWidth/2, start.y + BodyHeight, start.z));
         
         //Bottom right
-        points.Add(new Vector3(width/2, 0, 0));
-        
-        triangles = new int[6] {0, 1, 2, 2, 3, 0};
+        points.Add(new Vector3(start.x + BodyWidth/2, start.y, start.z));
 
+        //Bottom left of tip
+        points.Add(new Vector3(start.x - TipWidth/2, start.y + BodyHeight, start.z));
+
+        //Bottom right of tip
+        points.Add(new Vector3(start.x + TipWidth/2, start.y + BodyHeight, start.z));
+
+        //Top of tip
+        points.Add(new Vector3(start.x, start.y + BodyHeight + TipHeight, start.z));
+
+
+        //Adding some depth
+        //Bottom left
+        points.Add(new Vector3(start.x - BodyWidth/2, start.y, start.z + zOffset));
+        
+        //Top left
+        points.Add(new Vector3(start.x - BodyWidth/2, start.y + BodyHeight, start.z + zOffset));
+        
+        //Top right
+        points.Add(new Vector3(start.x + BodyWidth/2, start.y + BodyHeight, start.z + zOffset));
+        
+        //Bottom right
+        points.Add(new Vector3(start.x + BodyWidth/2, start.y, start.z + zOffset));
+
+        //Bottom left of tip
+        points.Add(new Vector3(start.x - TipWidth/2, start.y + BodyHeight, start.z + zOffset));
+
+        //Bottom right of tip
+        points.Add(new Vector3(start.x + TipWidth/2, start.y + BodyHeight, start.z + zOffset));
+
+        //Top of tip
+        points.Add(new Vector3(start.x, start.y + BodyHeight + TipHeight, start.z + zOffset));
+        
+        for(int i = 0; i < points.Count; i++){
+            points[i] = Quaternion.Euler(eulerAngleRotation) * points[i];
+        }
+        
+        
+        triangles = new int[48] {0, 1, 2, 
+                                2, 3, 0, 
+                                4, 6, 5,
+                                0, 7, 8,
+                                8, 1, 0,
+                                3, 2, 9,
+                                9, 10,3,
+                                6, 4,11,
+                                11,13,6,
+                                12,5, 6,
+                                6,13,12,
+                                10, 9,7,
+                                9, 8, 7,
+                                12,13,11,
+                                4, 5, 12,
+                                4, 12,11};
     }
 
 }
